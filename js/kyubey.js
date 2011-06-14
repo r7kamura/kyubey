8f7611ed615546d1764b78a77c7556e0503d5ff3
Ryo NAKAMURA <r7kamura@gmail.com>
2011-06-15 03:33:49 +0900

    README++

--- js/kyubey.js
+++ js/kyubey.js
@@ -2,23 +2,23 @@
   $.kyubey = function(args){
     var config = $.extend({animation: "fadeIn"}, args);
 
-    // 画面に合わせて高さを調整 
+    // 画面に合わせて高さを調整
     function sizing() {
       var h = window.innerHeight;
       var w = window.innerWidth;
       $("section").css("max-height", h + "px");
     };
 
-    // キーバインドの設定 
+    // キーバインドの設定
     function bindKey() {
       $(window).keyup(function(e){
-        if(e.keyCode==37) {         // ← 
+        if(e.keyCode==37) {         // ←
           goPrev();
-        } else if(e.keyCode==38) {  // ↑ 
+        } else if(e.keyCode==38) {  // ↑
           goFirst();
-        } else if(e.keyCode==39) {  // → 
+        } else if(e.keyCode==39) {  // →
           goNext();
-        } else if(e.keyCode==40) {  // ↓ 
+        } else if(e.keyCode==40) {  // ↓
           goLast();
         }
       });
@@ -35,7 +35,7 @@
       }
     };
 
-    // ページの移動 
+    // ページの移動
     function goPrev() {
       show($("section.current").prev());
     };
@@ -54,7 +54,7 @@
       return $("section.current .lazy:hidden:first").slideDown().length;
     }
 
-    // #page にページ番号を挿入 
+    // #page にページ番号を挿入
     function paging() {
       var pageNum = $("section").length;
       var i = 1;
@@ -76,7 +76,7 @@
         hatena.parse($(this).text());
         $(this).html(hatena.html());
       });
-      $(".section").each(function(){ // はてな記法は勝手にdiv.sectionを付けるので消す 
+      $(".section").each(function(){ // はてな記法は勝手にdiv.sectionを付けるので消す
         $(this).replaceWith($(this).html());
       });
       upHeadingLevel(selector, 2);
@@ -127,7 +127,7 @@
       $("#page").click(goFirst);
     };
 
-    // ページ読込み時用の初期化動作 
+    // ページ読込み時用の初期化動作
     function init() {
       sizing();
       hatenize();
